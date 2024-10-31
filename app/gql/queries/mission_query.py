c95ed234d183858ecb413c23aa3117ed1ed9791d
pgrp770 <pgrp770@gmail.com>
2024-10-31 11:50:27 +0200

    feat: add queries 1, 2, 3, 4

--- app/gql/queries/mission_query.py
+++ app/gql/queries/mission_query.py
@@ -1,7 +1,10 @@
-from graphene import ObjectType, Field, Int, List, Date
+from graphene import ObjectType, Field, Int, List, Date, String
 
 from app.db.database import session_maker
+from app.db.models.city import City
+from app.db.models.country import Country
 from app.db.models.mission import Missions
+from app.db.models.target import Target
 from app.gql.types.mission_type import MissionsType
 
 
@@ -22,4 +25,27 @@ class Query(ObjectType):
                     .filter(Missions.mission_date >= start_date and Missions.mission_date <= end_date)
                     .all())
 
+    mission_by_country = List(MissionsType, country=String(required=True))
+
+    @staticmethod
+    def resolve_mission_by_country(root, info, country):
+        with session_maker() as session:
+            return (session.query(Missions)
+                    .join(Target)
+                    .join(City)
+                    .join(Country)
+                    .filter(Country.country_name == country)
+                    .all())
+
+    mission_by_target_industry = List(MissionsType, target_industry=String(required=True))
+
+    @staticmethod
+    def resolve_mission_by_target_industry(root, info, target_industry):
+        with session_maker() as session:
+            return (session.query(Missions)
+                    .join(Target)
+                    .filter(Target.target_industry == target_industry)
+                    .all())
+
     
+
