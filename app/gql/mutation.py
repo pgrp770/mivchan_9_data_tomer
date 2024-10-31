0ea332dffa6386700df06a34c80c2c7441154676
pgrp770 <pgrp770@gmail.com>
2024-10-31 14:27:04 +0200

    feat: add migration update_mission

--- app/gql/mutation.py
+++ app/gql/mutation.py
@@ -1,9 +1,11 @@
 from graphene import ObjectType
 
-from app.gql.mutations.mission_mutation import AddMission
+from app.gql.mutations.mission_mutation import AddMission, UpdateMission
 from app.gql.mutations.target_mutation import AddTarget
 
 
 class Mutation(ObjectType):
     add_mission = AddMission.Field()
+    update_mission = UpdateMission.Field()
+    
     add_target = AddTarget.Field()
--- app/gql/mutations/mission_mutation.py
+++ app/gql/mutations/mission_mutation.py
@@ -1,10 +1,9 @@
-from graphene import Mutation, Date, Float, Field
+from graphene import Mutation, Date, Float, Field, Int
 
 from app.db.database import session_maker
 from app.db.models import Missions
 from app.gql.types.mission_type import MissionsType
 from sqlalchemy.sql.expression import func
-
 
 
 class AddMission(Mutation):
@@ -20,7 +19,6 @@ class AddMission(Mutation):
 
     mission = Field(MissionsType)
 
-
     @staticmethod
     def mutate(root,
                info,
@@ -31,8 +29,7 @@ class AddMission(Mutation):
                aircraft_returned,
                aircraft_failed,
                aircraft_damaged,
-        aircraft_lost):
-
+               aircraft_lost):
         with session_maker() as session:
             mission_id = session.query(func.max(Missions.mission_id)).scalar() + 1
             new_mission = Missions(
@@ -50,3 +47,48 @@ class AddMission(Mutation):
             session.commit()
             session.refresh(new_mission)
             return AddMission(mission=new_mission)
+
+
+class UpdateMission(Mutation):
+    class Arguments:
+        mission_id = Int(required=True)
+        mission_date = Date(required=True)
+        airborne_aircraft = Float(required=True)
+        attacking_aircraft = Float(required=True)
+        bombing_aircraft = Float(required=True)
+        aircraft_returned = Float(required=True)
+        aircraft_failed = Float(required=True)
+        aircraft_damaged = Float(required=True)
+        aircraft_lost = Float(required=True)
+
+    mission = Field(MissionsType)
+
+    @staticmethod
+    def mutate(root,
+               info,
+               mission_id,
+               mission_date,
+               airborne_aircraft,
+               attacking_aircraft,
+               bombing_aircraft,
+               aircraft_returned,
+               aircraft_failed,
+               aircraft_damaged,
+               aircraft_lost):
+        with session_maker() as session:
+            maybe_mission = session.query(Missions).filter(Missions.mission_id == mission_id).first()
+            if maybe_mission is None:
+                raise Exception("there is on mission with this id")
+
+            maybe_mission.mission_date = mission_date,
+            maybe_mission.airborne_aircraft = airborne_aircraft,
+            maybe_mission.attacking_aircraft = attacking_aircraft,
+            maybe_mission.bombing_aircraft = bombing_aircraft,
+            maybe_mission.aircraft_returned = aircraft_returned,
+            maybe_mission.aircraft_failed = aircraft_failed,
+            maybe_mission.aircraft_damaged = aircraft_damaged,
+            maybe_mission.aircraft_lost = aircraft_lost
+
+            session.commit()
+            session.refresh(maybe_mission)
+            return AddMission(mission=maybe_mission)
